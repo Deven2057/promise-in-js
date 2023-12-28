1be97750110d0a6c4a7b1cf817dc81da49500032
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-12-28 14:40:32 +0545

    Create multiHandlers.js

---
+++ multiHandlers.js
@@ -0,0 +1,13 @@
+let p1 =new Promise((resolve,reject)=>{
+  console.log("hey i am not resolved")
+  setTimeout(()=>{
+    resolve(1);
+  },2000)
+})
+
+p1.then(()=>{
+  console.log("congratulations this promise is now resolved")
+})
+p1.then(()=>{
+  console.log("hurrey")
+})
